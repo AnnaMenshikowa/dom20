bd0db3aeada844c0d587bb2180ecff37de922060
AnnaMenshikowa <130694230+AnnaMenshikowa@users.noreply.github.com>
2024-02-02 17:58:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace domasnyaa1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contactListBox = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(567, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(567, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(567, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Редактировать";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(13, 396);
+            this.phoneNumberTextBox.Multiline = true;
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(191, 38);
+            this.phoneNumberTextBox.TabIndex = 3;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(13, 331);
+            this.nameTextBox.Multiline = true;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(191, 39);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите имя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите номер телефона:";
+            // 
+            // contactListBox
+            // 
+            this.contactListBox.BackColor = System.Drawing.Color.White;
+            this.contactListBox.FormattingEnabled = true;
+            this.contactListBox.ItemHeight = 20;
+            this.contactListBox.Location = new System.Drawing.Point(12, 63);
+            this.contactListBox.Name = "contactListBox";
+            this.contactListBox.Size = new System.Drawing.Size(548, 204);
+            this.contactListBox.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(567, 104);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 43);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Поиск";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(567, 63);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(211, 35);
+            this.searchTextBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(296, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 27);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "СПИСОК КОНТАКТОВ";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button5.Location = new System.Drawing.Point(660, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(59, 36);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "CSV";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button6.Location = new System.Drawing.Point(725, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 37);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "TXT";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.contactListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox contactListBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace domasnyaa1
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+        public override string ToString()
+        {
+            return $"{Name} - {PhoneNumber}";
+        }
+    }
+    public partial class Form1 : Form
+    {
+        private List<Contact> contacts;
+        public Form1()
+        {
+            InitializeComponent();
+            contacts = new List<Contact>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string phoneNumber = phoneNumberTextBox.Text;
+
+            Contact contact = new Contact(name, phoneNumber);
+            contacts.Add(contact);
+            UpdateContactList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (contactListBox.SelectedItem != null)
+            {
+                Contact selectedContact = (Contact)contactListBox.SelectedItem;
+                contacts.Remove(selectedContact);
+                UpdateContactList();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (contactListBox.SelectedItem != null)
+            {
+                Contact selectedContact = (Contact)contactListBox.SelectedItem;
+                string name = nameTextBox.Text;
+                string phoneNumber = phoneNumberTextBox.Text;
+
+                selectedContact.Name = name;
+                selectedContact.PhoneNumber = phoneNumber;
+                UpdateContactList();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string searchName = searchTextBox.Text;
+            foreach (Contact contact in contacts)
+            {
+                if
+                    (contact.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase))
+                {
+                    contactListBox.SelectedItem = contact;
+                    return;
+                }
+            }
+            MessageBox.Show("Контакт не найден", "Результат поиска");
+        }
+        private void UpdateContactList()
+        {
+            contactListBox.DataSource = null;
+            contactListBox.DataSource = contacts;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Title = "Сохранить контакты в .txt файл",
+                CheckPathExists = true,
+                DefaultExt = "txt",
+                Filter = "Text files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    foreach (Contact contact in contacts)
+                    {
+                        writer.WriteLine($"{contact.Name} {contact.PhoneNumber}");
+                    }
+                }
+            }
+            MessageBox.Show("Контакты сохранены в файл .txt!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Title = "Сохранить контакты в .csv файл",
+                CheckPathExists = true,
+                DefaultExt = "csv",
+                Filter = "CSV files (*.csv)|*.csv",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    writer.WriteLine("Name, PhoneNumber");
+                    foreach (Contact contact in contacts)
+                    {
+                        writer.WriteLine($"{contact.Name} {contact.PhoneNumber}");
+                    }
+                }
+            }
+            MessageBox.Show("Контакты сохранены в файл .csv!");
+        }
+    }
+    
+}
